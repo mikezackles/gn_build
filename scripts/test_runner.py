2f9e67b96be3f1f622380cf4c66c17452dc34665
Zachary Michaels <mikezackles@gmail.com>
2016-03-02 09:52:25 -0500

    Account for tests that depend on working directory

--- scripts/test_runner.py
+++ scripts/test_runner.py
@@ -4,12 +4,12 @@ import os
 import sys
 import subprocess
 
-print(sys.argv)
-test_executable = os.path.abspath(sys.argv[1])
-stamp_path = os.path.abspath(sys.argv[2])
-command = [test_executable]
+test_dir = os.path.abspath(sys.argv[1])
+test_executable = sys.argv[2]
+stamp_path = os.path.abspath(sys.argv[3])
+os.chdir(test_dir)
 try:
-  subprocess.check_call(command)
+  subprocess.check_call([test_executable])
   with open(stamp_path, 'a'):
     os.utime(stamp_path, None)
 except:
