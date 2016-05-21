356d11b5bce94e356fa39c6709375655cae105b7
Zachary Michaels <mikezackles@gmail.com>
2016-05-21 11:52:27 -0400

    Add script for generating headers from glsl

---
+++ scripts/file_to_string_literal.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+
+with open(sys.argv[1], 'w') as outfile:
+    with open(sys.argv[2], 'r') as infile:
+        outfile.write('namespace {0} {{\n'.format(sys.argv[3]))
+        outfile.write('char const* {0} =\n'.format(sys.argv[4]))
+        outfile.write('"SHADER(\n')
+        outfile.write(infile.read())
+        outfile.write('\n)SHADER"\n')
+        outfile.write('}')
