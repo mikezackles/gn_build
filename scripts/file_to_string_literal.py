30d9de131b9d0ab10145628a3bdbbdb34ec918ed
Zachary Michaels <mikezackles@gmail.com>
2016-05-31 11:10:35 -0400

    Use std::string instead of char const* for shaders

--- scripts/file_to_string_literal.py
+++ scripts/file_to_string_literal.py
@@ -4,8 +4,9 @@ import sys
 
 with open(sys.argv[1], 'w') as outfile:
     with open(sys.argv[2], 'r') as infile:
+        outfile.write('#include <string>\n\n');
         outfile.write('namespace {0} {{\n'.format(sys.argv[3]))
-        outfile.write('char const* {0} =\n'.format(sys.argv[4]))
+        outfile.write('std::string const {0} =\n'.format(sys.argv[4]))
         outfile.write('R"SHADER(\n')
         outfile.write(infile.read())
         outfile.write('\n)SHADER";\n')
